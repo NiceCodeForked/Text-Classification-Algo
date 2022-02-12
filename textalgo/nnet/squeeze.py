ab70770b9bacb324c5fe92345dba11223cbe71db
penguinwang96825 <penguinwang@smail.nchu.edu.tw>
2022-02-12 23:19:32 +0800

    [feat] Add basic layers classes

---
+++ textalgo/nnet/squeeze.py
@@ -0,0 +1,21 @@
+import torch.nn as nn
+
+
+class Squeeze(nn.Module):
+
+    def __init__(self, dim):
+        super(Squeeze, self).__init__()
+        self.dim = dim
+
+    def forward(self, x):
+        return x.squeeze(self.dim)
+
+
+class Unsqueeze(nn.Module):
+
+    def __init__(self, dim):
+        super(Unsqueeze, self).__init__()
+        self.dim = dim
+
+    def forward(self, x):
+        return x.unsqueeze(self.dim)
