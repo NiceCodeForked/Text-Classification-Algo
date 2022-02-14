6a74bfa7b52fb74d273a500dfd90e517f243793f
penguinwang96825 <penguinwang@smail.nchu.edu.tw>
2022-02-14 13:36:21 +0000

    [feat] Add run_shell()

--- textalgo/utils.py
+++ textalgo/utils.py
@@ -1,5 +1,10 @@
 import argparse
+import logging
+import subprocess
 from .hyperpyyaml import load_hyperpyyaml
+
+
+logger = logging.getLogger(__name__)
 
 
 def load_yaml(filename):
@@ -83,3 +88,46 @@ def isint(value):
         return True
     except ValueError:
         return False
+
+
+def run_shell(cmd):
+    r"""This function can be used to run a command in the bash shell.
+    Arguments
+    ---------
+    cmd : str
+        Shell command to run.
+    Returns
+    -------
+    bytes
+        The captured standard output.
+    bytes
+        The captured standard error.
+    int
+        The returncode.
+    Raises
+    ------
+    OSError
+        If returncode is not 0, i.e., command failed.
+    Example
+    -------
+    >>> out, err, code = run_shell("echo 'hello world'")
+    >>> out.decode(errors="ignore")
+    'hello world\n'
+    """
+
+    # Executing the command
+    p = subprocess.Popen(
+        cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True
+    )
+
+    # Capturing standard output and error
+    (output, err) = p.communicate()
+
+    if p.returncode != 0:
+        raise OSError(err.decode(errors="replace"))
+
+    # Adding information in the logger
+    msg = output.decode(errors="replace") + "\n" + err.decode(errors="replace")
+    logger.debug(msg)
+
+    return output, err, p.returncode
