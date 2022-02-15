c8acf9d85eb5f5011a694b8ba3953ff8641aa7cf
penguinwang96825 <penguinwang@smail.nchu.edu.tw>
2022-02-15 15:43:30 +0000

    [refactor] Fix url path

--- textalgo/dataset/cfpb_dataset.py
+++ textalgo/dataset/cfpb_dataset.py
@@ -3,10 +3,10 @@ from pathlib import Path
 from datasets import load_dataset
 
 
-URL_PATH = 'https://media.githubusercontent.com/media/penguinwang96825/Text-Classification-Algo/master/textalgo/dataset/csv/'
+CFPB_URL = 'https://raw.githubusercontent.com/penguinwang96825/Text-Classification-Algo/master/data/'
 MODULE_PATH = Path(os.path.abspath(__file__)).parent
-TRAIN_CSV_PATH = URL_PATH + 'cfpb-train.csv'
-TEST_CSV_PATH = URL_PATH + 'cfpb-test.csv'
+TRAIN_CSV_PATH = CFPB_URL + 'cfpb-train.csv'
+TEST_CSV_PATH = CFPB_URL + 'cfpb-test.csv'
 
 
 def load(split='train'):
