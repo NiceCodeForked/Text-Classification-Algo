721918c13471487be6abbf3c3790f888f678b160
penguinwang96825 <penguinwang@smail.nchu.edu.tw>
2022-02-16 08:19:24 +0800

    [feat] Add SpatialDropout

--- textalgo/nnet/dropout.py
+++ textalgo/nnet/dropout.py
@@ -1,3 +1,4 @@
+import itertools
 import torch
 import torch.nn as nn
 
@@ -35,15 +36,52 @@ class AutoDropout(nn.Module):
 
 class SpatialDropout(nn.Module):
     """
+    Benefit of adding SpatialDropout over normal dropout layer is that 
+    in the SpatialDropout entire embedding channels are dropped while 
+    the normal embedding dropout drops all channels for entire words, 
+    and sometimes losing one or more words can alter the meaning completely.
+
+    Spatial dropout, i.e. dropout in the specified axis direction, 
+    often used after Embedding layer and CNN layer. For example, 
+    for the input of (batch, timesteps, embedding), if axis=1, a number of 
+    channels of the embedding can be dropout as a whole. If axis=2, then 
+    some tokens can be dropout as a whole.
+
     References
     ----------
     1. https://arxiv.org/pdf/1411.4280v3.pdf
     2. https://blog.csdn.net/weixin_43896398/article/details/84762943
+    3. https://blog.csdn.net/guofei_fly/article/details/108561847
+    4. https://www.kaggle.com/c/jigsaw-toxic-comment-classification-challenge/discussion/52058
     """
-    pass
-
-
-
+    def __init__(self, drop=0.5):
+        super(SpatialDropout, self).__init__()
+        self.drop = drop
+        
+    def forward(self, inputs, noise_shape=None):
+        outputs = inputs.clone()
+        if noise_shape is None:
+            noise_shape = (
+                inputs.shape[0], 
+                *itertools.repeat(1, inputs.dim()-2), 
+                inputs.shape[-1]
+            )
+        
+        self.noise_shape = noise_shape
+        if not self.training or self.drop == 0:
+            return inputs
+        else:
+            noises = self._make_noises(inputs)
+            if self.drop == 1:
+                noises.fill_(0.0)
+            else:
+                noises.bernoulli_(1 - self.drop).div_(1 - self.drop)
+            noises = noises.expand_as(inputs)    
+            outputs.mul_(noises)
+            return outputs
+            
+    def _make_noises(self, inputs):
+        return inputs.new().resize_(self.noise_shape)
 
 
 class GaussianDropout(nn.Module):
