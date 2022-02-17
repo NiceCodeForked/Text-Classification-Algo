fc9af888027dc22c5ea4ccb56a79fbed62352b69
penguinwang96825 <penguinwang@smail.nchu.edu.tw>
2022-02-17 01:11:10 +0000

    [feat] Add DynamicPadding and StaticPadding

---
+++ textalgo/collate.py
@@ -0,0 +1,36 @@
+import torch
+import torch.nn.functional as F
+
+
+class DynamicPadding(object):
+
+    def __init__(self, x_col_name, y_col_name):
+        super().__init__()
+        self.x_col_name = x_col_name
+        self.y_col_name = y_col_name
+
+    def __call__(self, batch):
+        labels = torch.tensor([b[self.y_col_name]for b in batch])
+        input_ids = [torch.tensor(b[self.x_col_name]) for b in batch]
+        input_ids = torch.nn.utils.rnn.pad_sequence(
+            input_ids, batch_first=True, padding_value=0
+        )
+        return {self.x_col_name:input_ids, self.y_col_name:labels}
+
+
+class StaticPadding(object):
+
+    def __init__(self, x_col_name, y_col_name, max_length):
+        super().__init__()
+        self.x_col_name = x_col_name
+        self.y_col_name = y_col_name
+        self.max_length = max_length
+
+    def __call__(self, batch):
+        labels = torch.tensor([b[self.y_col_name]for b in batch])
+        input_ids = [b[self.x_col_name] for b in batch]
+        input_ids = torch.stack([F.pad(i, (0, self.max_length-i.shape[0])) for i in input_ids])
+        return {
+            self.x_col_name: input_ids, 
+            self.y_col_name: labels
+        }
