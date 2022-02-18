4209b1c48ec916d3e56fd280dfaa8eb0e710ec44
penguinwang96825 <penguinwang@smail.nchu.edu.tw>
2022-02-18 16:57:02 +0000

    [shell] Run train and test script

--- recipe/cfpb/AttnTextCNN/run.sh
+++ recipe/cfpb/AttnTextCNN/run.sh
@@ -4,5 +4,8 @@ pythonpath="/share/mini1/sw/std/python/anaconda3-2020.11/2020.11/envs/demixing2/
 parentdir='/share/mini1/res/t/asr/studio/read-us/timit/demixing/nlp-algo'
 recipedir='recipe/cfpb/AttnTextCNN'
 
-execfile=$parentdir/$recipedir/train.py
-$pythonpath $execfile
+trexecfile=$parentdir/$recipedir/train.py
+ttexecfile=$parentdir/$recipedir/eval.py
+
+$pythonpath $trexecfile
+$pythonpath $ttexecfile
