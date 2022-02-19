d80f1feea9ce18ca911a781cd6dbe3ae4acde6f5
penguinwang96825 <penguinwang@smail.nchu.edu.tw>
2022-02-19 23:30:46 +0800

    [package] Add lemminflect and spacy

--- setup.sh
+++ setup.sh
@@ -2,7 +2,10 @@ pip install --user nltk
 python -c "import nltk; nltk.download('punkt')"
 python -c "import nltk; nltk.download('stopwords')"
 
+pip install -U pip setuptools wheel
 pip install -r requirements.txt
 
 pip install --upgrade gensim
+pip install -U spacy
 pip install -U scikit-learn
+python -m spacy download en_core_web_sm
